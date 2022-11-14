ad66899a4d1192814126fc3e6406fabb29ec331f
Ami <ami1996_tm@live.co.uk>
2022-11-14 15:32:25 +0000

    Added socket.io working room test

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,45 +1,71 @@
 import React, { useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom';
 
-import { io } from "socket.io-client"
+import io from "socket.io-client"
 const serverEndpoint = "http://127.0.0.1:5001";
-
-let socket = io(serverEndpoint);
-
-function handleClick(e) {
-  e.preventDefault()
-  socket.emit('counter clicked');
-}
+const socket = io(serverEndpoint);
 
 export default function Home() {
-  const [count, setCount] = useState(0); 
+  // const [socket, setSocket] = useState(null)
+  const [isConnected, setIsConnected] = useState(socket.isConnected)
+  const [inRoom, setInRoom] = useState(null)
+  
+  function submitForm(e) {
+    e.preventDefault()
+    const form = e.target
+    const data = Object.fromEntries(new FormData(form))
 
-  // useEffect(() =>{
-  //   socket.on('admin-message', msg => console.log(msg));
-  // },[])
+    socket.emit('create-new-room', data)
 
-  // const joinRoom = (e) => {
-  //   e.preventDefault()
-  //   socket.emit('request-join-room', {roomId: `test`})}
+    // form.reset()
+  }
+
   useEffect(() => {
-    socket.on('counter updated', function(countFromServer) {
-      setCount(countFromServer);
+
+    // setSocket({socket})
+
+    // setIsConnected(socket.connected)
+
+    socket.on('admin-message', (msg) => {
+      console.log(msg);
     })
-    
+
+    socket.on('connect', () => {
+      setIsConnected(true)
+    })
+
+    socket.on('disconnect', () => {
+      setIsConnected(false)
+    })
+
+    return () => {
+      socket.off('connect')
+      socket.off('disconnect')
+    };
   }, []);
 
   return (
     <div>
       <div>
-        <form>
+        <form name="createRoom" onSubmit={submitForm}>
           <label>Create User:</label>
           <input type="text"></input>
 
-          <button onClick={handleClick}>Create Room {count}</button>
+          <button>Create Room</button>
           <NavLink to="/Lobby">Lobby</NavLink>
 
-          <label>Join room</label>
-          <input type="text"></input>
+        </form>
+
+        <form name="joinRoom" onSubmit={submitForm}>
+          <label>
+            Room code
+            <input type="text" placeholder='Enter room code' name='roomCode' required></input>
+          </label>
+          <label>
+            Message
+            <input type="text" placeholder='Enter message' name='message' required></input>
+          </label>
+          <input type="submit"></input>
         </form>
       </div>
 
