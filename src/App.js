ea392603b02c8c87f823cccad761bfeb700d397e
Oliver Catelan-Robinson <oscr1998@outlook.com>
2022-11-15 10:08:55 +0000

    whos that pokemon title

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import {Home, Leaderboard, Lobby, Game} from './Pages'
 import {Routes, Route} from 'react-router-dom'
 import pokeball from './Components/images/pokeball.svg'
+import title from './Components/images/Who.png'
 import './App.css'
 export default function App() {
 
   return (
     <div className='App'>
+      <img class="title" src={title} alt="whos that pokemon title"></img>
       <div class="circlesContainer">
         <img class="circles" src={pokeball} alt="pokeball"></img>
         <img class="circles" src={pokeball} alt="pokeball"></img>
