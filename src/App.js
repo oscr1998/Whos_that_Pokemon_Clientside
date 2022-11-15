1d3456583443c7b77601587dc67bd0d6367a598f
Oliver Catelan-Robinson <oscr1998@outlook.com>
2022-11-15 09:30:30 +0000

    some styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import {Home, Leaderboard, Lobby, Game} from './Pages'
 import {Routes, Route} from 'react-router-dom'
-
+import pokeball from './Components/images/pokeball.svg'
+import './App.css'
 export default function App() {
 
   return (
-    <div>
+    <div className='App'>
+      <div class="circlesContainer">
+        <img class="circles" src={pokeball} alt="pokeball"></img>
+        <img class="circles" src={pokeball} alt="pokeball"></img>
+        <img class="circles" src={pokeball} alt="pokeball"></img>
+        <img class="circles" src={pokeball} alt="pokeball"></img>
+        <img class="circles" src={pokeball} alt="pokeball"></img>
+        <img class="circles" src={pokeball} alt="pokeball"></img>
+        <img class="circles" src={pokeball} alt="pokeball"></img>
+        <img class="circles" src={pokeball} alt="pokeball"></img>
+      </div>
+      
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/leaderboard" element={<Leaderboard/>}/>
