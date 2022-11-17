4524a99bd6f3a50c242f08d32574ac63397f1165
Ami <ami1996_tm@live.co.uk>
2022-11-17 17:00:55 +0000

    Commented local storage use in lobby component for later

--- src/Pages/Lobby/index.js
+++ src/Pages/Lobby/index.js
@@ -22,18 +22,14 @@ export default function Lobby() {
 
   useEffect(() => {
     // Get local storage and store state
-    const localStorageData = JSON.parse(window.localStorage.getItem('data'))
+    const localStorageData = JSON.parse(window.localStorage.getItem('state'))
 
     if(localStorageData){
       // Use local storage as source of truth
       // Maybe replace with backend database in the future
       dispatch(loadData(localStorageData))
-      setLocalStorage(localStorageData)
+      //setLocalStorage(localStorageData) // UNCOMMENT THIS LINE LATER
     }
-
-    console.log(room)
-
-    // If local storage is different from redux state
   }, [])
 
   // copy to clipboard
@@ -105,7 +101,9 @@ export default function Lobby() {
   ) 
   : (
     <div>
-      Wrong room! Are you looking for room {room.code}
+      <p>You're in the wrong place! {room.code && <> Are you looking for room {room.code}? </>}</p>
+      
+      {room.code && <button onClick={() => navigate(`/rooms/${room.code}`)}>Rejoin</button>}
       <button onClick={() => navigate('/')}>&larr; Go back home</button>
     </div>
   )
