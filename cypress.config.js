d17ba4a4107c27243f921e709587c72e1a2ed1f3
GimmeDaLute <peter.m.edwards@outlook.com>
2022-11-17 12:19:21 +0000

    Cypress Testing Odyssey part2:Reckoning

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,12 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  // setupNodeEvents can be defined in either
+  // the e2e or component configuration
+
+  viewportWidth: 1600,
+  viewportHeight: 900,
+
   e2e: {
     setupNodeEvents(on, config) {
       require('@cypress/code-coverage/task')(on, config)
@@ -9,13 +15,6 @@ module.exports = defineConfig({
       // It's IMPORTANT to return the config object
       // with any changed environment variables
       return config
-    },
-  },
-
-  "component": {
-    "devServer": {
-      "framework": "create-react-app",
-      "bundler": "webpack"
     }
   }
 })
