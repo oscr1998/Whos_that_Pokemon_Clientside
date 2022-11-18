76184625c376443d92bef65cd1201ff1ab79b997
GimmeDaLute <peter.m.edwards@outlook.com>
2022-11-18 09:44:18 +0000

    Testing Part 4: Agony - Coverage abandoned, resolving to fix last tests to present

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,15 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  // setupNodeEvents can be defined in either
-  // the e2e or component configuration
 
   viewportWidth: 1600,
   viewportHeight: 900,
   video: false,
+  screenshotOnRunFailure: false,
 
   e2e: {
     setupNodeEvents(on, config) {
       require('@cypress/code-coverage/task')(on, config)
-      // include any other plugin code...
-      
-      // It's IMPORTANT to return the config object
-      // with any changed environment variables
       return config
     }
   }
